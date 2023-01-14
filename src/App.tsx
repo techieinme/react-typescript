c10dd5346e376f247de2b081ea1b161d52c00df2
Lakshmikanth <lakshmikantharaju09@gmail.com>
2023-01-14 13:22:16 +0530

    resuable component'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Toast } from "./component/Toast";
+import { Button } from "./component/Button";
+import { Input } from "./component/Input";
+import { Text } from "./component/polymorphic/Text";
+import { Tag } from "./component/polymorphic/Tag";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toast position="center" />
+      {/* <Button
+        data-test="12"
+        variant="primary"
+        onClick={() => {
+          console.log("hello button click");
+        }}
+      >
+        Hello Button
+      </Button>
+      <Input type="text" placeholder="Hello text" data-test="asd" />
+      <Input type="password" placeholder="password type " />
+      <Text as="h1" size="sm" color="primary">
+        <p>primary</p>
+      </Text>
+      <Text as="p" size="md" color="secondary">
+        secondary
+      </Text>
+
+      <Text as="label" htmlFor="somid" size="md" color="secondary">
+        secondary
+      </Text> */}
+
+      <Tag as="h1" color="primary" size="md">
+        h1
+      </Tag>
+      <Tag
+        as="label"
+        htmlFor="test"
+        data-label="hello"
+        color="primary"
+        size="md"
+      >
+        label
+      </Tag>
+      <Tag as="span" color="primary" size="md">
+        span
+      </Tag>
     </div>
   );
 }
